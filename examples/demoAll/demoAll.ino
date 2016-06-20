ba0f15b95a04bdb965218f498fe98f3d00a3e459
mrmot021 <mrmot021@gmail.com>
2016-06-20 14:17:14 +0200

    Add files via upload

---
+++ examples/demoAll/demoAll.ino
@@ -0,0 +1,32 @@
+#include <KELshield.h>
+
+KELshield shield;
+
+void setup()
+{
+}
+
+void loop()
+{
+  shield.writeDisplay(LEDS, shield.readSwitches());
+  
+  if (shield.buttonState(LEFT) == HIGH)
+    shield.writeDisplay(D1, 0xf3); //'A'
+  else
+    shield.writeDisplay(D1, 0x01);  //'-'
+    
+  if (shield.buttonState(UP) == HIGH)
+    shield.writeDisplay(D2, 0x5b); //'b'
+  else
+    shield.writeDisplay(D2, 0x01);  //'-'
+    
+  if (shield.buttonState(DOWN) == HIGH)
+    shield.writeDisplay(D3, 0xd8); //'C'
+  else
+    shield.writeDisplay(D3, 0x01);  //'-'
+    
+  if (shield.buttonState(RIGHT) == HIGH)
+    shield.writeDisplay(D4, 0x3b); //'d'
+  else
+    shield.writeDisplay(D4, 0x01);  //'-'
+}
